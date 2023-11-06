d4a1ac1a7c39e9ec8ead4be6a583fa379d2f3080
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2023-11-06 07:24:28 +0700

    adding form visits, submission, and bounce rate interface

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -10,5 +10,28 @@ export default function Home() {
 
 async function CardStatsWrapper() {
   const stats = await GetFormStats();
-  return <StatsCard loading={false} data={stats} />
+  return <StatsCards loading={false} data={stats} />
+}
+
+interface StatsCardProps {
+  data: Awaited<ReturnType<typeof GetFormStats>>;
+  loading: boolean;
+}
+
+function StatsCards(props: StatsCardProps){
+  const{ data, loading } = props;
+
+  return <div className='
+    w-full
+    pt-8
+    gap-4
+    grid
+    grid-cols-1
+    md:grid-cols-2
+    lg:grid-cols-4
+  '>
+    <StatsCard
+      title="Total Visits"
+    />
+  </div>
 }
