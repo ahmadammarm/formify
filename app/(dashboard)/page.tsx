d0f1c14df153b05b9f201a7de50d0a9057240b77
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2023-11-06 21:17:05 +0700

    update form page

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -22,8 +22,8 @@ export default function Home() {
       <Suspense fallback={<StatsCards loading={true} />}>
         <CardStatsWrapper />
       </Suspense>
-      <Separator className="my-6" />
-      <h2 className="text-4xl font-bold col-span-2">Your forms</h2>
+      <Separator className="my-6 text-gray-500" />
+      <h2 className="text-4xl font-bold col-span-2">Your Forms</h2>
       <Separator className="my-6" />
       <div className="grid gric-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         <CreateFormBtn />
