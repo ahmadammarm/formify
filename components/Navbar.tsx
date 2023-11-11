f90aed4d0fe116831fbf7c4e4f335a08dbe1c897
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2023-11-11 07:50:48 +0700

    update navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,7 +4,6 @@ import React, { useState } from 'react';
 import Logo from './Logo';
 import ThemeSwitcher from './ThemeSwitcher';
 import { UserButton } from '@clerk/nextjs';
-import Link from 'next/link';
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -25,24 +24,7 @@ const Navbar = () => {
             &#9776;
           </button>
         </div>
-        <div
-          className={`${
-            isMenuOpen ? 'block' : 'hidden'
-          } md:flex md:items-center w-full max-w-md px-2 py-2 rounded-md shadow-md md:ml-6 md:justify-end md:w-1/2 lg:w-1/3 xl:w-1/4`}
-        >
-          <Link
-            href="/"
-            className="flex font-bold items-center justify-center w-full px-2 py-2 text-base text-center text-teal-500 transition duration-150 ease-in-out border border-transparent rounded-md hover:bg-gray-100 focus:outline-none focus:border-gray-300 focus:shadow-outline-gray md:ml-4 md:px-4 md:py-2 md:text-sm md:rounded-md"
-          >
-            Home
-          </Link>
-          <Link
-            href="/about"
-            className="flex items-center justify-center w-full px-2 py-2 text-base font-medium text-center text-teal-500 transition duration-150 ease-in-out border border-transparent rounded-md hover:bg-gray-100 focus:outline-none focus:border-gray-300 focus:shadow-outline-gray md:ml-4 md:px-4 md:py-2 md:text-sm md:rounded-md"
-          >
-            About
-          </Link>
-        </div>
+    
         <div className="flex gap-4 items-center">
           <ThemeSwitcher />
           <UserButton afterSignOutUrl="/sign-in" />
