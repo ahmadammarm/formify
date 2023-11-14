6934a461c9896a78135ff0b0c1f3ffdda351729d
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2023-11-14 17:29:04 +0700

    update navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -16,15 +16,6 @@ const Navbar = () => {
     <nav className="flex justify-between items-center border-b border-gray-500 h-[60px] px-4 py-2">
       <Logo />
       <div className="flex items-center">
-        <div className="md:hidden">
-          <button
-            className="mr-4 text-2xl text-gray-500 focus:outline-none"
-            onClick={toggleMenu}
-          >
-            &#9776;
-          </button>
-        </div>
-    
         <div className="flex gap-4 items-center">
           <ThemeSwitcher />
           <UserButton afterSignOutUrl="/sign-in" />
